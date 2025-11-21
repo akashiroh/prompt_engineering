5d7e7b9cc195f73756cb7d137b75872577ec5f2f
Andrew <holmesa8@wwu.edu>
2025-11-21 13:08:18 -0800

    modified sql llm agent to look at multiple tables in db

--- agent.py
+++ agent.py
@@ -1,12 +1,16 @@
 from llama_cpp import Llama
-import sqlite3
+from pathlib import Path
 import pandas as pd
+import argparse
+import sqlite3
 import yaml
+import time
 
-from llms.llm import LLMAgent
+from llm import LLMAgent
 
 def initialize_model(
     model_name: str="Qwen/Qwen2-7B-Instruct-GGUF",
+    n_ctx: int=2048,
 ):
     """return a pretrained llm from huggingface."""
 
@@ -21,53 +25,60 @@ def initialize_model(
         llm = Llama.from_pretrained(
             repo_id=model_name,
             filename="*q4_k_m.gguf",
-            n_ctx=2048,
+            n_ctx=n_ctx,
             n_gpu_layers=-1,
             verbose=False,
         )
         print(f"Downloaded model [{model_name}] from huggingface hub")
     return llm
 
-def load_db(db_name, table_name: str="yelp"):
+def load_db(db_name):
     """load sqlite db and return connection and schema."""
     conn = sqlite3.connect(db_name)
 
-    schema = ""
-    pragma = pd.read_sql_query(f"PRAGMA table_info({table_name});", conn)
-    schema = f"CREATE TABLE {table_name} (\n"
-    for _, r in pragma.iterrows():
-        schema += f"  {r['name']} {r['type']},\n"
-    schema = schema.rstrip(",\n") + "\n);"
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
 
-    return conn, schema
+    schemas = {k[0]: "" for k in tables}
+
+    for table_name in schemas.keys():
+        pragma = pd.read_sql_query(f"PRAGMA table_info({table_name});", conn)
+        schema = f"CREATE TABLE {table_name} (\n"
+        for _, r in pragma.iterrows():
+            schema += f"  {r['name']} {r['type']},\n"
+        schema = schema.rstrip(",\n") + "\n);"
+
+        schemas[table_name] = schema
+
+    schema_str =  "\n\n".join(schemas.values())
+    return conn, schemas, schema_str
 
 def query_db(conn, query):
     """use the llm query to return a response."""
     return conn.execute(query)
 
 
-def main():
+def main(args):
     """conversation loop."""
-    llm = initialize_model()
+
+    # load database(s)
+    conn, schemas, schema_str = load_db(
+        db_name=args.database,
+    )
+
+    examples = ""
+    for name in schemas.keys():
+        response = conn.execute("SELECT * FROM business LIMIT 3;")
+        examples += f"TABLE {name} \n"
+        examples += f"Examples: \n {'\n'.join(map(str, response))} \n"
+
+    llm = initialize_model(args.model)
 
     agents = {}
     with open("preconditions.yaml") as f:
         preconditions = yaml.safe_load(f)
         agents["sql-llm"] = LLMAgent(llm, preconditions["sql-llm"])
-
-    # load database
-    conn, schema = load_db(
-        db_name="sqlite_db/yelp_business.sqlite",
-        table_name="yelp",
-    )
-
-    # samples from database
-    db_samples = conn.execute(
-        "SELECT * from yelp WHERE categories LIKE '%restaurant%' LIMIT 2"
-    )
-    examples = "\n".join([
-        " ".join(map(str, sample)) for sample in db_samples
-    ])
 
     print("Query the LLM.")
     usr_msg = None
@@ -76,18 +87,39 @@ def main():
         usr_msg = input()
 
         sql_query = agents["sql-llm"].chat(
-            f"Here is the database schema: \n [{schema}] [{examples}] \n Here is the user message: " + usr_msg,
+            f"Here are the database schemas: \n [{schema_str}] \n Here is the user message: " + usr_msg,
         )
-        sql_response = query_db(conn, sql_query)
-
         print("\n[ASSISTANT]:", sql_query)
-        for row in sql_response:
-            print(row)
+
+        start_time = time.time()
+        sql_response = query_db(conn, sql_query)
+        end_time = time.time()
+
+        response = " | ".join([r[0] for r in sql_response])
+        print(response)
+        print(f"Query took {end_time - start_time:.3f} seconds")
 
         print()
 
 
+def parse_args():
+    """parse dem args."""
+    parser = argparse.ArgumentParser(description="Create a SQLite DB from a CSV or JSON.")
+
+    parser.add_argument(
+        "-m", "--model", type=str,
+        default="Qwen/Qwen2-7B-Instruct-GGUF",
+        help="HuggingFace Model",
+    )
+    parser.add_argument(
+        "-d", "--database", type=Path,
+        default="/research/hutchinson/workspace/holmesa8/data/sqlite/yelp.sqlite",
+        help="/path/to/*sqlite",
+    )
+
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    model_name = "Qwen/Qwen2.5-3B-Instruct-GGUF"
-    main()
+    args = parse_args()
+    main(args)
